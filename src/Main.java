a55d7d754ea0b33c78596df72544ed9f6de10b68
E-MAIL PRINCIPAL <mancusodavidelimamancuso@gmail.com>
2026-06-10 14:09:53 -0400

    feat: Adiciona conversão IP/Long e melhora organização do projeto

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ import Service.SubnetCalculator;
 void main() {
     SubnetCalculator calculator = new SubnetCalculator();
     SubnetInfo info = calculator.calcular("192.168.1.10/26");
+    System.out.println("Rede: " + info.getRede());
+    System.out.println("Broadcast: " + info.getBroadcast());
+    System.out.println("Primeiro Host: " + info.getPrimeiroHost());
+    System.out.println("Último Host: " + info.getUltimoHost());
     System.out.println("Hosts: " + info.getQuantidadeHost());
 }
--- src/Service/SubnetCalculator.java
+++ src/Service/SubnetCalculator.java
@@ -4,12 +4,12 @@ import Model.SubnetInfo;
 
 public class SubnetCalculator {
     public SubnetInfo calcular(String entrada) {
+
         //METHOD
 
         String[] partes = entrada.split("/");
         String ip = partes[0];
         int cidr = Integer.parseInt(partes[1]);
-        long hosts = calcularHost(cidr);
 
         // VALIDATION IP
 
@@ -17,11 +17,16 @@ public class SubnetCalculator {
             throw new IllegalArgumentException("IP inválido!");
         }
 
+        long hosts = calcularHost(cidr);
+        long ipLong = ipParaLong(ip);
+
         // CALCULATE HOST
 
         SubnetInfo info = new SubnetInfo();
+
         info.setQuantidadeHost(hosts);
         return info;
+
     }
 
     // VALIDATION METHOD
@@ -58,5 +63,26 @@ public class SubnetCalculator {
         if (cidr == 32) return 1;
         if (cidr == 31) return 2;
         return (1L << (32 - cidr)) - 2;
+    }
+
+    // CONVERT IP TO LONG
+
+    private long ipParaLong(String ip) {
+
+        String[] octetos = ip.split("\\.");
+        long resultado = 0;
+
+        for(String octeto: octetos) {
+            resultado = (resultado << 8)
+                    | Integer.parseInt(octeto); // Desloca 8 bits e adiciona o próximo octeto ao número final
+        }
+        return resultado;
+    }
+
+    private String longParaIp(long valor) {
+        return ((valor >> 24) & 255) + "." +
+                ((valor >> 16) & 255) + "." +
+                ((valor >> 8) & 255) + "." +
+                (valor & 255);
     }
 }
